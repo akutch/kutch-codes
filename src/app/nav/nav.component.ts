a68d3774a74c40867acaadbbf1415f5ca6599683
Anna Kutch <annakutch@gmail.com>
2017-07-16 18:23:37 -0400

    Fade in the nav too! #faded

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeInAnimation } from '../animations/fade-in-animation'
 
 @Component({
   selector: 'ak-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
+  animations: [fadeInAnimation],
+  host: {'[@fadeInAnimation]': ''}
 })
 export class NavComponent implements OnInit {
 
